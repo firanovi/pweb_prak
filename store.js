58b3c4b087d429e4565d570e9e74b6e0cfba7d8a
daniaveronika <daniapangaribuan@gmail.com>
2026-04-27 11:36:56 +0700

    perbaikan store

--- store.js
+++ store.js
@@ -1,38 +1,37 @@
 // ================= DATA PRODUK =================
 const productsSection1 = [
-    { name: "Batik Sumenep",         price: 500000, img: "./gambar/batik sumenep.jpg" },
-    { name: "Kue Macho",             price: 20000,  img: "./gambar/kue macho.png" },
-    { name: "Kacang Otok",           price: 15000,  img: "./gambar/kacangotok.jpeg" },
-    { name: "Buah Siwalan",          price: 35000,  img: "./gambar/buahsiwalan.jpg" },
-    { name: "Odheng",                price: 25000,  img: "./gambar/odheng.png" },
-    { name: "Miniatur Karapan Sapi", price: 400000, img: "./gambar/karapan sapi.jpg" },
+    { name: "Batik Sumenep",         price: 500000, img: "./gambar/batik sumenep.jpg",          href: "detailBatikSumenep.html" },
+    { name: "Kue Macho",             price: 20000,  img: "./img/kue macho.png",                 href: "detailKueMacho.html" },
+    { name: "Kacang Otok",           price: 15000,  img: "./gambar/kacangotok.jpeg",             href: "detailKacangOtok.html" },
+    { name: "Buah Siwalan",          price: 35000,  img: "./gambar/buahsiwalan.jpg",             href: "detailBuahSiwalan.html" },
+    { name: "Odheng",                price: 25000,  img: "./gambar/odheng.png",                  href: "detailOdheng.html" },
+    { name: "Miniatur Karapan Sapi", price: 400000, img: "./img/miniatur karapan sapi.png",      href: "detailMiniaturKarapanSapi.html" },
 ];
 
 const productsSection2 = [
-    { name: "Batik Sumenep",         price: 500000, img: "./gambar/batik sumenep.jpg" },
-    { name: "Kacang Otok",           price: 15000,  img: "./gambar/kacangotok.jpeg" },
-    { name: "Kue Macho",             price: 20000,  img: "./gambar/kue macho.png" },
-    { name: "Buah Siwalan",          price: 35000,  img: "./gambar/buahsiwalan.jpg" },
-    { name: "Odheng",                price: 25000,  img: "./gambar/odheng.png" },
-    { name: "Kaos Sakera",           price: 40000,  img: "./gambar/kaos sakera.jpeg" },
+    { name: "Keripik Tette",         price: 25000,  img: "./img/keripik tette.jpeg",             href: "detailKeripikTette.html" },
+    { name: "Petis Madura",          price: 20000,  img: "./img/petis madura.png",               href: "detailPetisMadura.html" },
+    { name: "Rengginang Lorjuk",     price: 30000,  img: "./img/rengginang lorjuk.png",          href: "detailRengginangLorjuk.html" },
+    { name: "Bolu Jubada",           price: 15000,  img: "./img/bolu jubada.png",                href: "detailBoluJubada.html" },
+    { name: "Keripik Terung",        price: 40000,  img: "./img/keripik terung.png",             href: "detailKeripikTerung.html" },
+    { name: "Kaos Sakera",           price: 40000,  img: "./gambar/kaos sakera.jpeg",            href: "detailKausSakera.html" },
 ];
 
 // ================= RENDER PRODUK =================
 function renderProducts(products, grids) {
-    // Bagi produk jadi 2 baris (3 per baris)
     const row1 = products.slice(0, 3);
     const row2 = products.slice(3, 6);
 
     [row1, row2].forEach((row, i) => {
         if (!grids[i]) return;
         grids[i].innerHTML = row.map(product => `
-            <div class="product-card">
+            <a href="${product.href}" class="product-card">
                 <div class="img-box">
                     <img src="${product.img}" alt="${product.name}">
                 </div>
                 <h3>${product.name}</h3>
                 <p>Rp ${product.price.toLocaleString('id-ID')}</p>
-            </div>
+            </a>
         `).join('');
     });
 }
